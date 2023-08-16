1663d336cb52ee6323a17cb8b8dbc6c593486973
RuriEnciso <rurienciso@gmail.com>
2023-08-16 18:33:01 -0500

    Contact content

--- modules/menuUI.js
+++ modules/menuUI.js
@@ -18,6 +18,14 @@ const showContact = () => {
   Booklist.style.display = 'none';
   AddBook.style.display = 'none';
   Contact.style.display = 'flex';
+  Contact.innerHTML = `
+  <p>Do have any questions or you just want to say "Hello"?
+  You can reach out to us!</p>
+  <ul>
+    <li>Our e-mail: mail@mail.com</li>
+    <li>Our phone number: 00*********</li>
+    <li>Our address: Streetname #,City, Pakistan, Peru & Venezuela.</li>
+  </ul>`;
 };
 
 export { showList, showAddBook, showContact };
